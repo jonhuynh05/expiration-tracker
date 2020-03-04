72f6095b2448cc9d3f6a266ec54747d6a13dd9e5
Jonathan Huynh <jonhuynh05@gmail.com>
2020-03-04 14:31:10 -0800

    connect home page

--- src/Home/index.js
+++ src/Home/index.js
@@ -1,1 +1,14 @@
-imrc
+import React, { Component } from 'react'
+import {withRouter} from "react-router-dom"
+
+class Home extends Component {
+    render(){
+        return(
+            <div>
+                this is the home page
+            </div>
+        )
+    }
+}
+
+export default withRouter(Home)
