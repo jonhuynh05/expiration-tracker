caba3e527535d5315b570f679cf3aa72f911d542
Jonathan Huynh <jonhuynh05@gmail.com>
2020-03-04 14:37:59 -0800

    create login and register inputs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import {withRouter, Switch, Route} from "react-router-dom"
 import Home from "./Home"
+import Register from "./Register"
 
 class App extends Component {
   render(){
@@ -8,6 +9,7 @@ class App extends Component {
       <div>
         <Switch>
           <Route exact path={"/"} render={() => <Home/>}/>
+          <Route exact path={"/register"} render={() => <Register/>}/>
         </Switch>
       </div>
     )
--- src/Register/index.js
+++ src/Register/index.js
@@ -1,21 +1,21 @@
 import React, { Component } from 'react'
 import {withRouter, Link} from "react-router-dom"
 
-class Home extends Component {
+class Register extends Component {
     render(){
         return(
             <div>
-                <div>Is Your Food Expired?</div>
-                <div>Login</div>
+                <div>Register</div>
                 <form>
+                    <input placeholder="First Name"></input>
+                    <input placeholder="Last Name"></input>
                     <input placeholder="Email"></input>
                     <input placeholder="Password"></input>
-                    <button type="submit">Login</button>
+                    <button type="submit">Register</button>
                 </form>
-                <div>Don't have an account? <Link to="/register">Register</Link></div>
             </div>
         )
     }
 }
 
-export default withRouter(Home)
+export default withRouter(Register)
